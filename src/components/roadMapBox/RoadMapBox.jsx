0051185918c00a6cee22fd5e888d908b3fd74bf4
michael beamer <beamermichael@gmail.com>
2022-01-02 23:12:33 +0100

    side navigation and folder structure

--- src/components/roadMapBox/RoadMapBox.jsx
+++ src/components/roadMapBox/RoadMapBox.jsx
@@ -4,14 +4,22 @@ import "./roadMapBox.scss";
 export default function RoadMapBox() {
   return (
     <div className="roadmap">
-      <h2 className="roadmap__title">Roadmap</h2>
-      <a className="roadmap__link" href="/">
-        View
-      </a>
+      <div className="roadmap__flex-container">
+        <h2 className="roadmap__title">Roadmap</h2>
+        <a className="roadmap__link" href="/">
+          View
+        </a>
+      </div>
       <ul className="roadmap__list">
-        <li className="roadmap__item planned">Planned </li>
-        <li className="roadmap__item progress">In-Progress</li>
-        <li className="roadmap__item live">Live</li>
+        <li className="roadmap__item planned">
+          Planned <span className="roadmap__item--quantity">2</span>
+        </li>
+        <li className="roadmap__item progress">
+          In-Progress <span className="roadmap__item--quantity">3</span>
+        </li>
+        <li className="roadmap__item live">
+          Live <span className="roadmap__item--quantity">1</span>
+        </li>
       </ul>
     </div>
   );
