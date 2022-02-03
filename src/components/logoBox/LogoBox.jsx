07932ba08fe056c44fc8e6c82f4aa6e659d7e2fe
michael beamer <beamermichael@gmail.com>
2022-02-03 10:45:06 +0100

    disabled scrolling while mobile menu is open

--- src/components/logoBox/LogoBox.jsx
+++ src/components/logoBox/LogoBox.jsx
@@ -10,9 +10,13 @@ export default function LogoBox() {
     setIsOpen((prevState) => !prevState);
 
     //disables scroll when modal/mobile-menu is open
+    // !isOpen
+    //   ? (document.body.style.overflow = "hidden")
+    //   : (document.body.style.overflow = "visible");
+
     !isOpen
-      ? (document.body.style.overflow = "hidden")
-      : (document.body.style.overflow = "visible");
+      ? (document.body.style.position = "fixed")
+      : (document.body.style.position = "relative");
   }
 
   return (
